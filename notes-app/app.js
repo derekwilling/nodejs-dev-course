6e3b7faa4efc7d991761838decd779e6baa71f14
DirtySoc <derek.willingham@outlook.com>
2019-03-28 17:43:28 -0400

    Completed lecture 16

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,46 @@
 const getNotes = require('./notes')
+const yargs = require('yargs')
+const chalk = require('chalk')
 
-console.log(getNotes())
+// Customize yargs version
+yargs.version('1.1.0')
 
-// const add = require('./utils')
-// console.log(add(2,-3))
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+// create the read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note!')
+    }
+})
+
+// create the list command
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: () => {
+        console.log('Listing the notes!')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
