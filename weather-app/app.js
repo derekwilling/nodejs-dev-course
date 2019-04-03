9520bfd1d1e79027e771b743083b3f83ad1216a3
DirtySoc <derek.willingham@outlook.com>
2019-04-03 16:41:04 -0400

    updates to api accessing in weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,27 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(()=>{
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/31c6091b3e385f767a6b40c9c80c03aa/37.8267,-122.4233'
 
-setTimeout(()=>{
-    console.log('0 second timer')
-}, 0)
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log('Error processing darksky request!')
+    } else if (response.body.error) {
+        console.log('Unable to find location.')
+    } else { 
+        console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is ${response.body.currently.precipProbability} chance of rain.`)
+    }
+})
 
-console.log('Stopping')
+// Geocoding
+const mapURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Woodstock%20GA.json?access_token=pk.eyJ1IjoiZGlydHlzb2MiLCJhIjoiY2p1MW1rbXp2MDM3djQzbG8wZDMweWJrcyJ9.Ci4FbZRJdLzZ1hZ96meH-w&limit=1"
+request({url: mapURL, json: true}, (error, response) => {
+    if (error) {
+        console.log('Error accessing mapbox api!')
+    } else if (response.body.features.length === 0) {
+        console.log('Location not found with mapbox!')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
+})
