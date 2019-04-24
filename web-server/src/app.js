37f9d6c962d9b9f50505d025740419b2a5ec5466
DirtySoc <derek.willingham@outlook.com>
2019-04-24 15:55:46 -0400

    completed lecture 45

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,28 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicPath = path.join(__dirname, '..//public')
 const port = 3000
 
+app.use(express.static(publicPath))
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.sendFile(path.join(publicPath, '//help.html'))
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.sendFile(path.join(publicPath, '//about.html'))
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page')
+    res.send({
+        forecast: 'Rainy with a chance of meatballs.',
+        locaiton: 'Narnia'
+    })
 })
 
 app.listen(port, () => console.log(`Express listening on port ${port}`))
