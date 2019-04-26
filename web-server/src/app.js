609bf77b644cb33175a2a77a24acabd4cb69f1b6
Derek Willingham <derek.willingham@outlook.com>
2019-04-26 00:42:06 -0400

    completed section 7

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -57,6 +57,7 @@ app.get('/help/*', (req, res) => {
 
 app.get('*', (req, res) => {
     res.render('404', {
+        title: '404',
         message:`We couldn't locate that page! Sorry!`,
         name: 'Derek Willingham'
     }) 
