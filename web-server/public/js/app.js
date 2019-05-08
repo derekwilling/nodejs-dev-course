f489e21b8e0467a326543802a9c887dad1bd9a53
Derek Willingham <derek.willingham@outlook.com>
2019-05-07 23:59:21 -0400

    completed lecture 58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,30 @@
-console.log('Hello from client side app.js!')
+// console.log('Hello from client side app.js!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data)
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
-fetch('http://localhost:3000/weather?address=Woodstock').then((response)=>{
-    response.json().then((data)=>{
-        if (data.error){
-            console.log('Error: ', data.error)
-        } else {
-            console.log('Location: ', data.location)
-            console.log('Forecast: ', data.forecast)
-        }
+const locationInput = document.querySelector('input')
+const weatherForm = document.querySelector('form')
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const location = locationInput.value
+    // console.log(locationInput.value)
+    // console.log(e.target[0].value)
+
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log('Error: ', data.error)
+            } else {
+                console.log('Location: ', data.location)
+                console.log('Forecast: ', data.forecast)
+            }
+        })
     })
+
 })
