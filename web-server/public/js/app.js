94836b2dccbe675e8f02e2c9f96abb9174a874b8
Derek Willingham <derek.willingham@outlook.com>
2019-05-07 23:36:44 -0400

    completed lecture 57

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Hello from client side app.js!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=Woodstock').then((response)=>{
+    response.json().then((data)=>{
+        if (data.error){
+            console.log('Error: ', data.error)
+        } else {
+            console.log('Location: ', data.location)
+            console.log('Forecast: ', data.forecast)
+        }
+    })
+})
