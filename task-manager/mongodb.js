9a3c15533e2faa1deedeb473185818ad5823d1b7
Derek Willingham <derek.willingham@outlook.com>
2019-05-10 22:44:44 -0400

    completed lecture 76

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,19 +1,62 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+const id = new ObjectID()
+console.log(id)
+
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to DB')
-    } 
+    }
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Derek',
-        age: 23
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Derek',
+    //     age: 23
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     }
+    //     console.log(result.ops)
+    // })
 
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Bill',
+    //         age: 44
+    //     }, {
+    //         name: 'Jordan',
+    //         age: 21
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log(error)
+    //     }
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Wash clothes.' ,
+    //         completed: false
+    //     }, {
+    //         description: 'Cook lunch.',
+    //         completed: false
+    //     }, {
+    //         description: 'Mow grass.',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Error inserting documents into collection.')
+    //     }
+    //     console.log(result.ops)
+    // })
 })
