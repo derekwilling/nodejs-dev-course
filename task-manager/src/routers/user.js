75919e67db5582b39590aedcd5cd448429555a74
Derek Willingham <derek.willingham@outlook.com>
2019-05-21 00:00:50 -0400

    completed lecture 105

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -7,8 +7,17 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send()
+        res.status(201).send(user)
     } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (error) {
         res.status(400).send()
     }
 })
@@ -47,7 +56,10 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(_id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+        
         if (!user) {
             return res.status(404).send()
         }
