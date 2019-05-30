6227cb2f47c6a02fdef4d97c179f7035444417fa
DirtySoc <derek.willingham@outlook.com>
2019-05-30 17:16:43 -0400

    completed lecture 133

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,16 +1,18 @@
 const express = require('express')
-const User = require('../models/user')
-const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendFarewell } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
-        const token = await user.generateAuthToken()
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
+        const token = await user.generateAuthToken()
         res.status(201).send({
             user,
             token
@@ -82,6 +84,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
+        sendFarewell(req.user.email)
         res.send(req.user)
     } catch (error) {
         res.status(500).send()
