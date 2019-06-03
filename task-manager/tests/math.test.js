222abec2372a3cf2c2d78f21bf155de00b1747b9
Derek Willingham <derek.willingham@outlook.com>
2019-06-02 23:49:58 -0400

    completed lecture 139

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,19 @@
-test('Hello world!', () => {
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, .3)
+    expect(total).toBe(13)
 })
 
-test ('This should fail', () => {
-    throw new Error('Fail!')
+test('Calc total with default tip', () => {
+    const total = calculateTip(10)
+    expect(total).toBe(12)
+})
+
+test('Should convert 32 F to 0 C', () => {
+    expect(fahrenheitToCelsius(32)).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    expect(celsiusToFahrenheit(0)).toBe(32)    
 })
